fbae14baebb7b0e4bc87d19c353fd90b83c607cd
Paul Murphy <paul.murphy@domain.com.au>
2018-10-29 18:52:11 +1100

    module 3-4 - adding lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,10 +3,18 @@ console.log('Starting app.');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
+const _ = require('lodash');
 
 const username = os.userInfo().username;
 
 const res = notes.addNote();
+
+console.log(`isString(true): ${_.isString(true)} `)
+console.log(`isString(1): ${_.isString(1)} `)
+console.log(`isString('hello'): ${_.isString('hello')} `)
+
+var filteredArray = _.uniq(['Paul', 'Jim', 'Paul', 1, 2, 3]);
+console.log(filteredArray);
 
 console.log(`Adding 3 + 4 = ${notes.add(3,4)}`);
 
