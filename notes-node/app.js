885e32d4eed5f9c24f370e44fc34ec92ae626cff
Paul Murphy <paul.murphy@domain.com.au>
2018-10-30 03:57:12 +1100

    Modules 3-6 - process user input

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+console.log(process.argv);
+var command = process.argv[2];
+console.log(`command: ${command}`);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading notes');
+} else if (command === 'remove') {
+    console.log('Remove notes');
+} else {
+    console.log('Command not recognized')
+}
