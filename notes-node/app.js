1ded8d4976dec107b14e5c7ca53b29e88669d9d5
Paul Murphy <paul.murphy@domain.com.au>
2018-10-29 19:04:30 +1100

    Module 3-5 - adding nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,29 +1,6 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes');
 const _ = require('lodash');
 
-const username = os.userInfo().username;
-
-const res = notes.addNote();
-
-console.log(`isString(true): ${_.isString(true)} `)
-console.log(`isString(1): ${_.isString(1)} `)
-console.log(`isString('hello'): ${_.isString('hello')} `)
-
-var filteredArray = _.uniq(['Paul', 'Jim', 'Paul', 1, 2, 3]);
-console.log(filteredArray);
-
-console.log(`Adding 3 + 4 = ${notes.add(3,4)}`);
-
-console.log(`res: ${res}`);
-
-fs.appendFile('greetings.txt', `Hello ${username}!\n`, (err) => {
-    if (err) {
-        console.log('Unable to write to file');
-    } else {
-        console.log('Done');
-    }
-});
+const notes = require('./notes');
