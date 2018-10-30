2679ca148fd7c36ca59034c5ba6744465fbafdbd
Paul Murphy <paul.murphy@domain.com.au>
2018-10-30 11:48:49 +1100

    Module 4-14 - advanced promises

--- weather-app/playground/promises.js
+++ weather-app/playground/promises.js
@@ -1,3 +1,27 @@
+const asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+
+    }, 1500);
+  });
+}
+
+const asyncDouble = (num) => {
+  return new Promise((resolve, reject) => {
+    if (typeof num === 'number') {
+      resolve(num * 2);
+    } else {
+      reject('Arguments must be numbers');
+    }
+
+  });
+}
+
 const somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     reject('unable to fulfill');
@@ -7,6 +31,13 @@ const somePromise = new Promise((resolve, reject) => {
 somePromise
   .then(d => console.log(d))
   .catch(e => console.log(`Error: ${e}`));
+
+asyncAdd(5, 4)
+  // .then(data => console.log(`Result: ${data}`))
+  .then( d => asyncAdd(d, 5))
+  .then( d => asyncDouble(d))
+  .then(d => console.log(`Result: ${d}`))
+  .catch(err => console.log(`Error: ${err}`));
 
   // somePromise
   //   .then((d) => {
