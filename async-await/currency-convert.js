8f7c845b1b3a8655bea91c2f491b7cb8010653ef
Paul Murphy <paul.murphy@domain.com.au>
2018-11-01 18:12:39 +1100

    Module 10-03.5 - part 2

--- async-await/currency-convert.js
+++ async-await/currency-convert.js
@@ -1,30 +1,15 @@
 const axios = require('axios');
-/**
- * 
- * @param {string} from currency code you want to convert from
- * @param {string} to currency code you want to convert from
- * @param {number} amount you have to convery 
- */
-const convert = (from, to, amount) => {
-  return 22.1
-}
-
-//fixer.io
-// 941933897aa8eb0d4f2c08a62e7c780a
-// http://data.fixer.io/api/latest\?access_key\=941933897aa8eb0d4f2c08a62e7c780a
 
 const api_key = process.env.FIXER_API_KEY;
 const url = `http://data.fixer.io/api/latest?access_key=${api_key}`;
 
-// Using promises
-// const getExchangeRate = (from, to) => {
-//   return axios.get(url).then(res => {
-//     const euro = 1 / res.data.rates[from];
-//     const rate = euro * res.data.rates[to]; 
-//     return rate;
-//   })
-// };
-
+/**
+ * Retrieves the exchange rate between from country code and to country code.
+ * 
+ * @param {string} from currency code
+ * @param {string} to currency code
+ * @returns {number} the exchange rate.
+ */
 const getExchangeRate = async (from, to) => {
   const response = await axios.get(url);
   const euro = 1 / response.data.rates[from];
@@ -32,7 +17,12 @@ const getExchangeRate = async (from, to) => {
   return rate;
 };
 
-
+/**
+ * Retrieves a list of countries where the currency is accepted.
+ * 
+ * @param {string} currencyCode 
+ * @return {array} of countries where current is accepted.
+ */
 const getCountries = async (currencyCode) => {
   const url = `https://restcountries.eu/rest/v2/currency/${currencyCode}`
   const response = await axios.get(url);
@@ -40,13 +30,21 @@ const getCountries = async (currencyCode) => {
   return response.data.map(country => country.name);
 };
 
+/**
+ * Converts the amount between the from country and the to country and displays countries
+ * where the to currrency is accepted.
+ * 
+ * @param {string} from currency code you want to convert from
+ * @param {string} to currency code you want to convert from
+ * @param {number} amount you have to convery 
+ */
+const convert = async(from, to, amount) => {
+  const convertedAmount = await getExchangeRate(from, to).then(rate => (amount * rate).toFixed(2));
+  const countries = await getCountries(to);
 
-getCountries("EUR")
-  .then(d => console.log(d))
+  return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries}`;
+}
+
+convert('USD', 'EUR', 20)
+  .then(data => console.log(data))
   .catch(err => console.log(err));
-
-// getExchangeRate('USD', 'CAD').then(rate => {
-//   console.log(rate);
-// }).catch(err => {
-//   console.log(err);
-// });
