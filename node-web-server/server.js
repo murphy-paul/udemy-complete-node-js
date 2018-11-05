14c3d64e26c68d59beb0691afae27c4cb1211b8d
Paul Murphy <paul.murphy@domain.com.au>
2018-11-06 07:48:01 +1100

    Module 5-05 - advanced templating

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,22 +3,29 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 
 app.get('/', (req, res) => {
   // res.send('<H1>Hello, express!</H1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Hello, world!',
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
