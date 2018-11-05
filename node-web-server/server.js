5971e5dad9b8792da0f8b2a97ea1232538b90f33
Paul Murphy <paul.murphy@domain.com.au>
2018-11-06 08:04:31 +1100

    Module 5-06 - middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}:\t${req.method}\t${req.url}`
+
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
