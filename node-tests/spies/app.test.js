c858fe602ea3ec8b3cc874e60c9f4118e1968ae0
Paul Murphy <paul.murphy@domain.com.au>
2018-11-07 17:23:58 +1100

    Module 6-09 - spies and rewire

---
+++ node-tests/spies/app.test.js
@@ -0,0 +1,36 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const rewire = require('rewire');
+
+chai.use(sinonChai);
+
+const expect = chai.expect;
+
+const app = rewire('./app');
+
+
+describe('App', () => {
+  // setup our mock
+  var db = {
+    saveUser: sinon.fake()
+  };
+  app.__set__('db', db);
+
+  it('it should call the spy', () => {
+    var spy = sinon.fake();
+
+    spy('Paul', 21);
+
+    expect(spy).to.have.been.calledWith('Paul', 21);
+  });
+
+  it('should call save user', () => {
+    const email = 'a@b.com';
+    const psswd = 'password';
+
+    app.handleSignup(email, psswd);
+
+    expect(db.saveUser).to.have.been.called; //With({ email, psswd });
+  })
+})
